03fabd11453d3e80a45203e95b3a2f60d6d83f24
jassem-manita <jasemmanita00@gmail.com>
2026-02-25 02:29:24 +0100

    update tests

--- tests/test_asr.py
+++ tests/test_asr.py
@@ -1,34 +1,33 @@
 import pytest
 import sympy as sp
 from parsing_fortran import asr
+import json
 
 
 def test_extract_from_asr_basic():
-    asr_str = '''
-    (RealConstant)
-        2.0
-    (Var 1 x)
-    Mul
-    (RealConstant)
-        3.0
-    Add
-    '''
-    cons, vars, ops = asr.extract_from_asr(asr_str)
+    
+    with open("tests/full_asr.json") as f:
+        asr_json = json.load(f)
+    body = asr_json["fields"]["symtab"]["fields"]["test"]["fields"]["body"]
+    assignment = body[0]["fields"]["value"]
+    cons, variables, ops = asr.extract_from_asr(assignment)
     assert cons == [2.0, 3.0]
-    assert 'x' in vars
+    assert 'x' in variables
     assert 'Mul' in ops and 'Add' in ops
+
 
 
 def test_build_sympy_expr():
     cons = [2.0, 3.0]
-    vars_ = ['x', 'y']
-    expr = asr.build_sympy_expr(cons, vars_)
+    variables = ['x', 'y']
+    expr = asr.build_sympy_expr(cons, variables)
     x = sp.Symbol('x', real=True)
     assert expr == 2.0 * x + 3.0
 
 
+
 def test_build_sympy_expr_fail():
     cons = [2.0]
-    vars_ = ['y']
-    expr = asr.build_sympy_expr(cons, vars_)
+    variables = ['y']
+    expr = asr.build_sympy_expr(cons, variables)
     assert expr is None
